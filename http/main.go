c15b385b92c89be4be63c20b19391c82b5e45145
mgiks <lidamil6785@gmail.com>
2026-04-12 19:52:59 +0600

    feat: add in-memory user creation and persistance

--- http/main.go
+++ http/main.go
@@ -4,18 +4,6 @@ import (
 	"net/http"
 )
 
-type api struct {
-	addr string
-}
-
-func (a api) getUsersHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Sent a list of users"))
-}
-
-func (a api) createUserHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Created user"))
-}
-
 func main() {
 	api := api{addr: ":8080"}
 
@@ -29,5 +17,8 @@ func main() {
 	mux.HandleFunc("GET /users", api.getUsersHandler)
 	mux.HandleFunc("POST /users", api.createUserHandler)
 
-	s.ListenAndServe()
+	err := s.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
 }
