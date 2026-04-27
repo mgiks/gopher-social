631df507ad50ee664f5663330016dd040eea9852
mgiks <lidamil6785@gmail.com>
2026-04-27 22:40:36 +0600

    feat: create package to get environment variables

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,10 +1,14 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/mgiks/gopher-social/internal/env"
+)
 
 func main() {
 	cfg := config{
-		addr: ":8080",
+		addr: env.GetString("PORT", ":8080"),
 	}
 
 	app := application{config: cfg}
