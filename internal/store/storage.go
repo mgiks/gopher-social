5d36a14e6cb9597a2ca895505bfb78f9ef19913d
mgiks <lidamil6785@gmail.com>
2026-04-28 00:49:44 +0600

    feat: create storage implementation

---
+++ internal/store/storage.go
@@ -0,0 +1,22 @@
+package store
+
+import (
+	"context"
+	"database/sql"
+)
+
+type Store struct {
+	Posts interface {
+		Create(context.Context) error
+	}
+	Users interface {
+		Create(context.Context) error
+	}
+}
+
+func NewStore(db *sql.DB) Store {
+	return Store{
+		Posts: &PostsStore{db: db},
+		Users: &UsersStore{db: db},
+	}
+}
