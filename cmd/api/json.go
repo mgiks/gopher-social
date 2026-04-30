a067afcfd11db4754dc1784f4ade3c8f61b405ce
mgiks <lidamil6785@gmail.com>
2026-05-01 02:14:56 +0600

    feat: add function to read json

--- cmd/api/json.go
+++ cmd/api/json.go
@@ -10,3 +10,13 @@ func writeJSON(w http.ResponseWriter, status int, data any) error {
 	w.WriteHeader(status)
 	return json.NewEncoder(w).Encode(data)
 }
+
+func readJSON(w http.ResponseWriter, r *http.Request, data any) error {
+	maxBytes := 1_024_578 // 1MB
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
+
+	decoder := json.NewDecoder(r.Body)
+	decoder.DisallowUnknownFields()
+
+	return decoder.Decode(data)
+}
