a511e9c8086b8e3ecff429ee2ff664e63cc919eb
mgiks <lidamil6785@gmail.com>
2026-05-01 00:41:47 +0600

    fix: use proper username and port for db url, use proper format for maxIdleTime

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -12,10 +12,10 @@ func main() {
 	cfg := config{
 		port: env.GetString("PORT", ":8080"),
 		db: dbConfig{
-			url:          env.GetString("DB_URL", "postgres://user:adminpassword@localhost/gopher-social?sslmode=disable"),
+			url:          env.GetString("DB_URL", "postgres://admin:adminpassword@localhost:5434/gopher-social?sslmode=disable"),
 			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
 			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
-			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15min"),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
 		},
 	}
 
