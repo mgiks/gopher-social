c5c28f319009de651a459f242cdbb4381aed0624
mgiks <lidamil6785@gmail.com>
2026-05-01 03:01:59 +0600

    feat: add handler to create posts

---
+++ cmd/api/posts.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/mgiks/gopher-social/internal/store"
+)
+
+type CreatePostPayload struct {
+	Title   string   `json:"title"`
+	Content string   `json:"content"`
+	Tags    []string `json:"tags"`
+}
+
+func (app *application) createPostHandler(w http.ResponseWriter, r *http.Request) {
+	var payload CreatePostPayload
+	if err := readJSON(w, r, &payload); err != nil {
+		writeJSONError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	post := &store.Post{
+		Title:   payload.Title,
+		Content: payload.Content,
+		// TODO: Change after auth
+		Tags:   payload.Tags,
+		UserID: 1,
+	}
+
+	if err := app.store.Posts.Create(r.Context(), post); err != nil {
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	if err := writeJSON(w, http.StatusCreated, post); err != nil {
+		writeJSONError(w, http.StatusInternalServerError, err.Error())
+	}
+}
