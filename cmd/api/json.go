0b0cacf481629efba65dcfdff172cccf025e882a
mgiks <lidamil6785@gmail.com>
2026-05-03 00:04:34 +0600

    feat: add json-response helper with envelope wrapper

--- cmd/api/json.go
+++ cmd/api/json.go
@@ -7,6 +7,13 @@ import (
 	"github.com/go-playground/validator/v10"
 )
 
+func (app application) jsonResponse(w http.ResponseWriter, status int, data any) error {
+	type envelope struct {
+		Data any `json:"data"`
+	}
+	return writeJSON(w, status, envelope{Data: data})
+}
+
 func writeJSON(w http.ResponseWriter, status int, data any) error {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(status)
