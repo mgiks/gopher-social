7ea5c3360206821b941323d945766e001d9084a0
mgiks <lidamil6785@gmail.com>
2026-05-04 22:28:49 +0600

    fix: use passed error instead of predefined in conflict response function

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -22,5 +22,5 @@ func (app application) notFoundResponse(w http.ResponseWriter, r *http.Request, 
 
 func (app application) conflictResponse(w http.ResponseWriter, r *http.Request, err error) {
 	log.Printf("conflict error: %s path: %s error: %s", r.Method, r.URL.Path, err.Error())
-	writeJSONError(w, http.StatusConflict, "resource state conflict")
+	writeJSONError(w, http.StatusConflict, err.Error())
 }
