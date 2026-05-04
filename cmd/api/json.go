f11dae6f5f0695f340199412fbc86cb3fb3704aa
mgiks <lidamil6785@gmail.com>
2026-05-05 01:34:22 +0600

    refactor: give a better name to read json function parameter

--- cmd/api/json.go
+++ cmd/api/json.go
@@ -20,14 +20,14 @@ func writeJSON(w http.ResponseWriter, status int, data any) error {
 	return json.NewEncoder(w).Encode(data)
 }
 
-func readJSON(w http.ResponseWriter, r *http.Request, data any) error {
+func readJSON(w http.ResponseWriter, r *http.Request, dest any) error {
 	maxBytes := 1_024_578 // 1MB
 	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
 
 	decoder := json.NewDecoder(r.Body)
 	decoder.DisallowUnknownFields()
 
-	return decoder.Decode(data)
+	return decoder.Decode(dest)
 }
 
 func writeJSONError(w http.ResponseWriter, status int, message string) error {
