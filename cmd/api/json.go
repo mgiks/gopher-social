aada35c7997a394eda894c6010f2857edb872028
mgiks <lidamil6785@gmail.com>
2026-05-05 01:37:33 +0600

    chore: use proper value for a megabyte in read json function

--- cmd/api/json.go
+++ cmd/api/json.go
@@ -21,7 +21,7 @@ func writeJSON(w http.ResponseWriter, status int, data any) error {
 }
 
 func readJSON(w http.ResponseWriter, r *http.Request, dest any) error {
-	maxBytes := 1_024_578 // 1MB
+	maxBytes := 1_048_576 // 1MB
 	r.Body = http.MaxBytesReader(w, r.Body, int64(maxBytes))
 
 	decoder := json.NewDecoder(r.Body)
