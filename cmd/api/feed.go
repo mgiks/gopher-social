7bb992ea65ed30c97dddc562edd3fe03176452d5
mgiks <lidamil6785@gmail.com>
2026-05-08 01:35:52 +0600

    fix: put feed in the appropriate tags and make swagger request succeed with default parameter values

--- cmd/api/feed.go
+++ cmd/api/feed.go
@@ -10,13 +10,13 @@ import (
 //
 //	@Summary		Gets an authenticated user's feed
 //	@Description	Gets a user's feed
-//	@Tags			users
+//	@Tags			feed
 //	@Accept			json
 //	@Produce		json
 //	@Param			limit	query		int			false	"Post limit"				minimum(1)				maximum(20)	default(20)
 //	@Param			offset	query		int			false	"Post offset"				minimum(0)				default(0)
 //	@Param			sort	query		string		false	"Sorting order"				Enums(asc, desc)		default(desc)
-//	@Param			tags	query		[]string	false	"Tags to search by"			collectionFormat(csv)	maxlength(5)	default([])	example(["new", "post"])
+//	@Param			tags	query		[]string	false	"Tags to search by"			collectionFormat(csv)	maxlength(5)	example("new", "post")
 //	@Param			search	query		string		false	"Seach query"				maxlength(100)
 //	@Param			since	query		string		false	"Post creation date cutoff"	format(date-time)	example(2006-01-02T15:04:05Z)
 //	@Param			until	query		string		false	"Post creation date cutoff"	format(date-time)	example(2006-01-02T15:04:05Z)
