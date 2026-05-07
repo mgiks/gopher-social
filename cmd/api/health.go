60513bdd7061bca58cd9e51de5facafd1a523027
mgiks <lidamil6785@gmail.com>
2026-05-08 01:36:19 +0600

    feat: add swagger documentation to health endpoint

--- cmd/api/health.go
+++ cmd/api/health.go
@@ -4,13 +4,29 @@ import (
 	"net/http"
 )
 
-func (app application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	data := map[string]string{
-		"status":  "ok",
-		"env":     app.config.env,
-		"version": apiVersion,
-	}
+type HealthData struct {
+	Status  string `json:"status"`
+	Env     string `json:"env"`
+	Version string `json:"version"`
+}
 
+// HealthCheck godoc
+//
+//	@Summary		Returns a healthcheck
+//	@Description	Returns a healthcheck
+//	@Tags			ops
+//	@Accept			json
+//	@Produce		json
+//	@Success		200	{object}	HealthData
+//	@Failure		500	{object}	apiError
+//	@Security		ApiKeyAuth
+//	@Router			/health [get]
+func (app application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	data := HealthData{
+		Status:  "ok",
+		Env:     app.config.env,
+		Version: apiVersion,
+	}
 	if err := writeJSON(w, http.StatusOK, data); err != nil {
 		app.internalServerError(w, r, err)
 	}
