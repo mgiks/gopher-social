84246b9f6f42de8cf007998bd7a2a8db424b3503
mgiks <lidamil6785@gmail.com>
2026-05-07 20:23:20 +0600

    feat: add swagger support

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+
+	"github.com/mgiks/gopher-social/docs" // This is required to generate swagger docs
 	"github.com/mgiks/gopher-social/internal/store"
+	httpSwagger "github.com/swaggo/http-swagger/v2"
 )
 
 type application struct {
@@ -16,9 +20,10 @@ type application struct {
 }
 
 type config struct {
-	port string
-	db   dbConfig
-	env  string
+	port   string
+	db     dbConfig
+	env    string
+	apiURL string
 }
 
 type dbConfig struct {
@@ -39,6 +44,9 @@ func (app application) mount() http.Handler {
 
 	r.Route("/v1", func(r chi.Router) {
 		r.Get("/health", app.healthCheckHandler)
+
+		docsURL := fmt.Sprintf("%s/swagger/doc.json", app.config.port)
+		r.Get("/swagger/*", httpSwagger.Handler(httpSwagger.URL(docsURL)))
 
 		r.Route("/posts", func(r chi.Router) {
 			r.Post("/", app.createPostHandler)
@@ -71,6 +79,10 @@ func (app application) mount() http.Handler {
 }
 
 func (app application) run(mux http.Handler) error {
+	docs.SwaggerInfo.Version = apiVersion
+	docs.SwaggerInfo.Host = app.config.apiURL
+	docs.SwaggerInfo.BasePath = "/v1"
+
 	srv := &http.Server{
 		Addr:         app.config.port,
 		Handler:      mux,
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -8,11 +8,28 @@ import (
 	"github.com/mgiks/gopher-social/internal/store"
 )
 
-const apiVersion = "0.0.1"
+const apiVersion = "0.0.2"
 
+//	@title			GopherSocial API
+//	@description	API for GopherSocial, a social network for gophers
+//	@termsOfService	http://swagger.io/terms/
+
+//	@contact.name	API Support
+//	@contact.url	http://www.swagger.io/support
+//	@contact.email	support@swagger.io
+
+//	@license.name	Apache 2.0
+//	@license.url	http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @BasePath					/v1
+//
+// @securityDefinitions.apiKey	ApiKeyAuth
+// @in							header
+// @name						Authorization
 func main() {
 	cfg := config{
-		port: env.GetString("PORT", ":8080"),
+		port:   env.GetString("PORT", ":8080"),
+		apiURL: env.GetString("EXTERNAL_URL", "localhost:8080"),
 		db: dbConfig{
 			url:          env.GetString("DB_URL", "postgres://admin:adminpassword@localhost:5434/gopher-social?sslmode=disable"),
 			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
